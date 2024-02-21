999357d8b3d3dcac559959dc0bbcb285ec84568a
FrancaTM <francatm@outlook.com>
2024-02-21 14:06:39 -0300

    modify to read text from file

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import api_key
 
 client = OpenAI(api_key=api_key.api_key)
 
-input_text = input("Enter text to be summarized: ")
+input_text = ""
+
+with open("text.txt", "r") as file:
+    input_text = file.read()
+    print("\nText in file: \n\n", input_text, "\n")
 
 response = client.chat.completions.create(
     model="gpt-4-turbo-preview",
